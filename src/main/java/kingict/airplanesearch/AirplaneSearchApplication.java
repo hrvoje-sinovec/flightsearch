83c85241f8c4e7cf0068eb77006ba3732ed17be6
hrvoje.sinovec <hrvoje.sinovec.ets@gmail.com>
2023-01-19 10:47:03 +0100

    Stranica se otvara na startu aplikacije

--- src/main/java/kingict/airplanesearch/AirplaneSearchApplication.java
+++ src/main/java/kingict/airplanesearch/AirplaneSearchApplication.java
@@ -3,9 +3,22 @@ package kingict.airplanesearch;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.awt.*;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+
 @SpringBootApplication
 public class AirplaneSearchApplication {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         SpringApplication.run(AirplaneSearchApplication.class, args);
+        openHomePage();
+    }
+
+    private static void openHomePage() throws IOException {
+        Runtime rt = Runtime.getRuntime();
+        rt.exec("rundll32 url.dll,FileProtocolHandler " + "http://localhost:7000");
     }
 }
+
